624d304b20e4f9e24f24cfe7d0582b7299b780cd
elemonier <emily.lemonier@gmail.com>
2013-05-10 17:54:21 -0400

    Added role attribute to users. Set Emily Lemonier to role: Admin from mongo command line.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,15 @@ class User
   field :first_name, type: String
   field :last_name, type: String
   field :email, type: String
-  
-  has_and_belongs_to_many :courses  
+  field :role, type: String
+  after_initialize :init
+
+  has_and_belongs_to_many :courses 
+
+  def init
+  	self.role ||= 'Student'
+  end
+
   
   #attr_accessible :email
   #validates_uniqueness_of :email
